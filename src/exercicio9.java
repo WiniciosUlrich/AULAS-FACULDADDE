a3edd13cb063335766f6bb80d996edfaf53535da
Winicios Ulrich <winiciosivanulrich10@gmail.com>
2023-03-12 01:25:47 -0300

    sistema de dolar

--- src/exercicio9.java
+++ src/exercicio9.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class exercicio9 {
     public static void main(String[] args)
     {
+        Scanner teclado = new Scanner(System.in);  
+
+        System.out.println("INSIRA A QUANTIDADE DE DOLARES");
         
+        Double quantdolar = teclado.nextDouble();
 
+        System.out.println("INSIRA A COTAÇÃO DO DOLAR");
 
+        float cotadolar = teclado.nextFloat();
+
+        System.out.println("O valor a ser pago em reais é de: "+quantdolar * cotadolar);
+
+        teclado.close();
 
     }
 
